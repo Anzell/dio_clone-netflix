2e57e8f4916b60e59dc494cb6bc8a4a81d28d116
Anzell <andrielferreira@outlook.com.br>
2021-04-03 17:41:00 -0300

    Adicionado modal para trailer e sinopse dos filmes. Arrumado responsividade

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $('.owl-carousel').owlCarousel({
         0: {
             items:1
         },
-        600: {
+        700: {
             items:3
         },
         1000: {
@@ -14,3 +14,39 @@ $('.owl-carousel').owlCarousel({
         }
     }
 });
+
+var modal = document.getElementById("modal");
+var botaoFecharModal = document.getElementById("botaoFecharModal")
+
+document.querySelectorAll(".filme").forEach(botao => {
+    botao.addEventListener("click", () => {
+        modal.style.display = "block";
+        var trailerUrl = botao.attributes['trailerUrl']['value'];
+        document.getElementById("conteudoModal").insertAdjacentHTML("beforeend", 
+            `<iframe id="trailer" width="100%" height="400px"  src="${trailerUrl}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+        );
+    });
+});
+
+document.querySelectorAll(".maisInformacoesFilme").forEach(botao => {
+    botao.addEventListener("click", () => {
+        modal.style.display = "block";
+        var sinopseFilme = botao.attributes['sinopseFilme']['value'];
+        document.getElementById("conteudoModal").insertAdjacentHTML("beforeend", 
+            `<p id = "sinopseFilmeExpandido">${sinopseFilme}</p>`
+        );
+    });
+});
+
+botaoFecharModal.onclick = function() {
+    modal.style.display = "none";
+    var trailer = document.getElementById("trailer");
+    var sinopse = document.getElementById("sinopseFilmeExpandido");
+    if(trailer != null){
+        trailer.remove();
+    }
+    if(sinopse != null){
+        sinopse.remove();
+    }  
+};
+
